d8e4802a6a7037dc678e96d8aa1ea2405daf5a43
Bob Burbach <bob.burbach@gmail.com>
2010-03-27 17:28:37 -0700

    Initial commit to cloud_kicker.

---
+++ test/helper.rb
@@ -0,0 +1,10 @@
+require 'rubygems'
+require 'test/unit'
+require 'shoulda'
+
+$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
+$LOAD_PATH.unshift(File.dirname(__FILE__))
+require 'cloud_kicker'
+
+class Test::Unit::TestCase
+end
