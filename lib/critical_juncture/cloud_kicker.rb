2a127cfa799e562a53e36c45f07624835943d42a
Bob Burbach <bob.burbach@gmail.com>
2010-03-28 23:48:06 -0700

    require files in the appropriate order so everything loads correctly

--- lib/critical_juncture/cloud_kicker.rb
+++ lib/critical_juncture/cloud_kicker.rb
@@ -1,5 +1,4 @@
-require 'map'
-require 'control_elements'
+require File.join(File.dirname(__FILE__), 'control_elements.rb')
 
 Dir["#{File.join(File.dirname(__FILE__), 'map', '*.rb')}"].each do |filename|
   require filename
@@ -8,3 +7,5 @@ end
 Dir["#{File.join(File.dirname(__FILE__), 'markers', '*.rb')}"].each do |filename|
   require filename
 end
+
+require File.join(File.dirname(__FILE__), 'map.rb')
