453b878e406692e1c3c382123c826d10d908b292
Bob Burbach <bob.burbach@gmail.com>
2010-03-28 18:34:19 -0700

    Require the rest of the files

--- lib/critical_juncture/cloud_kicker.rb
+++ lib/critical_juncture/cloud_kicker.rb
@@ -1,3 +1,10 @@
+require 'map'
+require 'control_elements'
+
 Dir["#{File.join(File.dirname(__FILE__), 'map', '*.rb')}"].each do |filename|
+  require filename
+end
+
+Dir["#{File.join(File.dirname(__FILE__), 'markers', '*.rb')}"].each do |filename|
   require filename
 end
